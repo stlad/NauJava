64540579b96dff0626c0cc94740dfb9122e3d92c
Vaganov Vladislav <vaganovlad@gmail.com>
2024-09-29 12:49:22 +0500

    init NauJava Project

---
+++ NauJava/src/test/java/ru/naumen/vagnov/naujava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumen.vagnov.naujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
