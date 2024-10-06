68b40a317a636a7bf2dfad03c44494ba00b2d8c7
Vaganov Vladislav <vaganovlad@gmail.com>
2024-10-06 11:47:45 +0500

    удаление не нужных классов

--- NauJava/src/main/java/ru/naumen/vagnov/naujava/exceptions/NaumenEntityExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.naumen.vagnov.naujava.exceptions;
-
-public class NaumenEntityExistsException extends RuntimeException{
-    public NaumenEntityExistsException(String message){
-        super(message);
-    }
-}
--- NauJava/src/main/java/ru/naumen/vagnov/naujava/exceptions/NaumenEntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.naumen.vagnov.naujava.exceptions;
-
-public class NaumenEntityNotFoundException extends RuntimeException{
-    public NaumenEntityNotFoundException(String message){
-        super(message);
-    }
-}
